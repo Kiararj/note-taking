fba47456a2e9c9323c7d85688279ec63ae826d1f
Kiara Johnson <kiarajohnson91@gmail.com>
2023-09-19 19:55:54 -0500

    fixed serger.js file

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const path = require('path');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
